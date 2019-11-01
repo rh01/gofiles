d50bdac1a50eee339ee9edd93bcf7b36f68bb4d9
rh01 <1048157315@qq.com>
2019-11-01 21:41:08 +0800

    add new code

--- lcode100-199/ex151/findDisappearedNumbers.go
+++ lcode100-199/ex151/findDisappearedNumbers.go
@@ -3,7 +3,6 @@
 // license that can be found in the LICENSE file.
 package ex151
 
-
 func abs(num int) int {
 	if num < 0 {
 		return -num
@@ -14,20 +13,19 @@ func abs(num int) int {
 func findDisappearedNumbers(nums []int) []int {
 	for i := 0; i < len(nums); i++ {
 		if nums[i] > 0 {
-			nums[abs(nums[i])-1] = -abs(nums[abs(nums[i])-1]);
+			nums[abs(nums[i])-1] = -abs(nums[abs(nums[i])-1])
 		}
 	}
 
 	res := []int{}
 
 	for i := 0; i < len(nums); i++ {
-		if nums[i] < 0 {
-			append(res, nums)
+		if nums[i] > 0 {
+			res = append(res, nums[i]+1)
 		}
 	}
+	return res
 }
-
-
 
 /*
 给定一个范围在  1 ≤ a[i] ≤ n ( n = 数组大小 ) 的 整型数组，数组中的元素一些出现了两次，另一些只出现一次。
